6f96e3248de9348ed3e3f97bf7d82efb1dabb7dc
Animus PEXUS <animuspexus@protonmail.com>
2020-03-24 02:05:18 +0300

    fix

--- githubcom_animuspexus_py_cloudpaymentsru/client.py
+++ githubcom_animuspexus_py_cloudpaymentsru/client.py
@@ -1,62 +1,76 @@
 import json
+import base64
+
 import urllib.request
 
 
 class Client:
-
-    def __init__(self, public_id: str = "", api_secret: str = "", anonymous_login: bool = False):
+    def __init__(
+            self,
+            basic_auth_user: str = "",
+            basic_auth_password: str = ""
+    ):
         self.base_uri = 'https://api.cloudpayments.ru/'
 
-        self.public_id = public_id
-        self.api_secret = api_secret
+        self.authorization_header = base64.b64encode(
+            "{}:{}".format(basic_auth_user, basic_auth_password).encode(encoding="utf-8")
+        ).decode(encoding="utf-8")
 
-    def SendAsJSON(self, obj: dict, to: str) -> (dict, Exception):
+    def SendAsJSON(self, obj: dict, to: str) -> (dict, [Exception]):
 
-        json_string = ""
+        exceptions = list()
 
         try:
             json_string = json.dumps(obj)
         except Exception as err:
-            return None, err
+            exceptions.append(err)
+            return None, exceptions
 
         req = None
         try:
             req = urllib.request.Request(
                 self.base_uri + to,
-                data=bytes(json_string, encoding='utf-8'),
+                data=json_string.encode(encoding='utf-8'),
                 headers={'Content-Type': 'application/json'}
             )
         except Exception as err:
-            return None, err
+            exceptions.append(err)
+            return None, exceptions
+
+        req.add_header("Authorization", "Basic {}".format(self.authorization_header))
 
         request_res = None
         try:
             request_res = urllib.request.urlopen(req)
         except Exception as err:
-            return None, err
+            exceptions.append(err)
+            return None, exceptions
 
         if request_res != 200:
-            return None, Exception
+            exceptions.append(Exception("Response is not 200"))
 
         resp_data = ""
         try:
             resp_data = request_res.read()
         except Exception as err:
-            return None, err
+            exceptions.append(err)
+            return None, exceptions
 
         resp_obj = None
         try:
             resp_obj = json.loads(resp_data)
         except Exception as err:
-            return None, err
+            exceptions.append(err)
+            return None, exceptions
 
         if not type(resp_obj) == dict:
-            return None, Exception("response is not JSON")
+            exceptions.append(Exception("response is not JSON"))
+            return None, exceptions
 
-        return resp_obj, None
+        return resp_obj, exceptions
 
-    def Test(self) -> (dict, Exception):
+    def Test(self) -> (dict, [Exception]):
         return self.SendAsJSON(dict(), "test")
 
-    def PerformSingleIterationPayment(self, payment: dict):
-        self.SendAsJSON(payment, "payments/cards/charge")
+    def PerformSingleIterationPayment(self, payment: dict) -> (dict, [Exception]):
+        return self.SendAsJSON(payment, "payments/cards/charge")
