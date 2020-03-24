139fedde10d2e009c08fa1dc12357841f1ab607f
Animus PEXUS <animuspexus@protonmail.com>
2020-03-24 03:34:07 +0300

    fix

--- githubcom_animuspexus_py_cloudpaymentsru/client.py
+++ githubcom_animuspexus_py_cloudpaymentsru/client.py
@@ -17,9 +17,6 @@ class Client:
             "{}:{}".format(basic_auth_user, basic_auth_password).encode(encoding="utf-8")
         ).decode(encoding="utf-8")
 
-        print("user", basic_auth_user)
-        print("password", basic_auth_password)
-
     def SendAsJSON(self, obj: dict, to: str) -> (dict, [Exception]):
 
         exceptions = list()
