211d345764325e186be9d6a942c03187f6e8d88c
Animus PEXUS <animuspexus@protonmail.com>
2020-03-24 03:54:11 +0300

    fix

--- githubcom_animuspexus_py_cloudpaymentsru/client.py
+++ githubcom_animuspexus_py_cloudpaymentsru/client.py
@@ -17,13 +17,13 @@ class Client:
             "{}:{}".format(basic_auth_user, basic_auth_password).encode(encoding="utf-8")
         ).decode(encoding="utf-8")
 
-    def SendAsJSON(self, obj: dict, to: str) -> (dict, [Exception]):
+    def SendAsJSON(self, obj: dict, to: str, method: str = 'GET') -> (dict, [Exception]):
 
         exceptions = list()
 
         try:
             request_res = requests.request(
-                method="POST",
+                method=method,
                 url=self.base_uri + to,
                 auth=(self.basic_auth_user, self.basic_auth_password,),
                 json=obj,
